395e1ba280ab0132d98e25041b5fdd304838b2c1
yeen000 <chynn0816@gmail.com>
2025-04-04 11:51:34 +0900

    🚀 Initial commit: Tori AB Test Dashboard

---
+++ pages_3_Causal_Inference.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("🤖 예측 페이지")
+    st.write("여기서 모델 예측 결과를 보여줍니다.")
---
+++ pages_4_MAB.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("🎯 MAB 결과 대시보드")
+    st.write("이곳은 MAB(Epsilon-Greedy, Thompson Sampling 등) 실험 결과를 보여주는 페이지입니다.")
